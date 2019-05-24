97fbed0d0b13ba42ce0ecacac1d6084fa9e93015
Tyler Barnett <tyler.barnett@me.com>
2019-05-23 19:05:53 -0600

    initial

--- GetAxisSettings/Program.cs
+++ GetAxisSettings/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace GetAxisSettings
 {
@@ -6,7 +8,30 @@ namespace GetAxisSettings
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+
+      try
+      {
+        //Enter the VAPIX command. Here we use param.cgi with the list action. 192.168.0.90 is the IP address of the Axis product.
+        var ParamList = "http://192.168.0.90/axis-cgi/param.cgi?action=list&responseformat=rfc";
+        NetworkCredential networkCredential = new NetworkCredential("root", "password");
+        WebRequest request = WebRequest.Create(ParamList);
+        request.Credentials = networkCredential;
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        Stream streamResponse = response.GetResponseStream();
+        StreamReader streamRead = new StreamReader(streamResponse);
+        Char[] readBuff = new Char[256000];
+        int count = streamRead.Read(readBuff, 0, 256000);
+        Console.WriteLine(new string(readBuff, 0, count));
+        count = streamRead.Read(readBuff, 0, 256000);
+        // Close and release the response.
+        streamRead.Close();
+        streamResponse.Close();
+        response.Close();
+      }
+      catch (Exception es)
+      {
+        Console.WriteLine(es.ToString(), "\nError Message");
+      }
     }
   }
 }
